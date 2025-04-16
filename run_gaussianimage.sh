5ed575736a468a00c1e65c7d23a86b5c71d7c858
Tan Chia Qian <66521590+TCQian@users.noreply.github.com>
2025-04-16 08:46:09 +0800

    Modify gaussianimage.sh

--- run_gaussianimage.sh
+++ run_gaussianimage.sh
@@ -1,21 +1,32 @@
-#!/bin/sh
+#!/bin/bash
+#SBATCH --job-name=GaussianVideo_HoneyBee          # Job name
 #SBATCH --gres=gpu:h100-47:1
 #SBATCH --time=2:00:00
 #SBATCH --mem=16G
+#SBATCH --mail-type=ALL                  # Get email for all status updates
+#SBATCH --mail-user=e0407638@u.nus.edu   # Email for notifications
+
+source ~/.bashrc
+conda activate gv
 
 echo "Starting..."
 
 # Define variables for easy updating.
-DATA_NAME="Beauty"
+DATA_NAME="HoneyBee"
 MODEL_NAME="GaussianImage_Cholesky"
 NUM_POINTS=10000
 TRAIN_ITERATIONS=20000
 QUANT_ITERATIONS=10000
 
 # Define dataset and checkpoint paths using the variables.
-DATASET_PATH="/home/l/leejiayi/GaussianVideo/dataset/${DATA_NAME}/"
-CHECKPOINT_PATH="/home/l/leejiayi/GaussianVideo/checkpoints/${DATA_NAME}/${MODEL_NAME}_${TRAIN_ITERATIONS}_${NUM_POINTS}/"
-CHECKPOINT_QUANT_PATH="/home/l/leejiayi/GaussianVideo/checkpoints_quant/${DATA_NAME}/${MODEL_NAME}_${QUANT_ITERATIONS}_${NUM_POINTS}/"
+YUV_PATH="/home/e/e0407638/github/GaussianVideo/YUV/${DATA_NAME}_1920x1080_120fps_420_8bit_YUV.yuv"
+DATASET_PATH="/home/e/e0407638/github/GaussianVideo/dataset/${DATA_NAME}/"
+CHECKPOINT_PATH="/home/e/e0407638/github/GaussianVideo/checkpoints/${DATA_NAME}/${MODEL_NAME}_${TRAIN_ITERATIONS}_${NUM_POINTS}/"
+CHECKPOINT_QUANT_PATH="/home/e/e0407638/github/GaussianVideo/checkpoints_quant/${DATA_NAME}/${MODEL_NAME}_${QUANT_ITERATIONS}_${NUM_POINTS}/"
+
+if [ ! -d "${DATASET_PATH}" ]; then
+    python utils.py "${YUV_PATH}" --width 1920 --height 1080 --start_frame ${START_FRAME} --num_frames ${NUM_FRAMES}
+fi
 
 # Run the training script with the required arguments.
 python train.py \
